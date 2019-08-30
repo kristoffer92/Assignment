9d858363de6bfe4144b410302db22771592347e0
kristoffer92 <kristoffer.borgs@hotmail.com>
2019-08-30 15:15:25 +0200

    commit test

--- src/main/java/se/Lexicon/App.java
+++ src/main/java/se/Lexicon/App.java
@@ -1,14 +1,13 @@
 package se.Lexicon;
 
 import java.util.Scanner;
-
 public class App<Public>
 {
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args ) {
         int operator = -1;
         boolean loop = true;
-        
+
         while(loop) {
             boolean gotResult = true;
             int result = 0;
@@ -39,19 +38,23 @@ public class App<Public>
                     System.out.println("Error");
             }//End of Switch
             if (gotResult && loop) {
-                System.out.println("Your result\n " + result);
-            }//End of Switch
+                System.out.println("Your result\n " + result); }
         }//End of While
     }//End of Main
     public static int getNumber(String Name){
         System.out.println("Enter " + Name + " number ");
         return scanner.nextInt(); }
-    public static int makeAddition(int numOne, int numTwo){
+    public static int makeAddition(int numOne, int numTwo) {
         return numOne+numTwo; }
     public static int makeSubtraction(int numOne, int numTwo){
         return numOne-numTwo; }
     public static int makeDivision(int numOne, int numTwo){
-        return numOne/numTwo; }
+        if(numTwo==0){
+            System.out.println("You can't divide by Zero");
+            return 0; }
+        else {
+            return numOne / numTwo; }
+        }//End of makeDivision
     public static int makeMultiplication(int numOne, int numTwo){
         return numOne*numTwo; }
     public static int makeModulus(int numOne, int numTwo){
--- src/test/java/se/Lexicon/AppTest.java
+++ src/test/java/se/Lexicon/AppTest.java
@@ -1,20 +1,45 @@
 package se.Lexicon;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+//Arrange, //Act, //Assert
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @Test //Dividing by Zero (Division)
+    public void test_Division(){
+        int numOne = 5; int numTwo = 0;
+        int result = App.makeDivision(numOne, numTwo);
+        Assert.assertEquals(0, result);
+    }
+    @Test//Testing Addition
+    public void test_makeAddition(){
+        int numOne = 3; int numTwo = 5;
+        int result = App.makeAddition(numOne, numTwo);
+        Assert.assertEquals(8, result);
+    }
+    @Test//Testing Subtraction
+    public void test_makeSubtraction(){
+        int numOne = 5; int numTwo = 2;
+        int result = App.makeSubtraction(numOne, numTwo);
+        Assert.assertEquals(3, result);
+    }
+    @Test//Testing Division
+    public void test_makeDivision(){
+        int numOne = 10; int numTwo = 2;
+        int result = App.makeDivision(numOne, numTwo);
+        Assert.assertEquals(5, result);
+    }
+    @Test//Testing Multiplication
+    public void test_Multiplication(){
+        int numOne = 5; int numTwo = 2;
+        int result = App.makeMultiplication(numOne, numTwo);
+        Assert.assertEquals(10, result);
+    }
+    @Test//Testing Modulus
+    public void test_Modulus(){
+        int numOne = 12; int numTwo = 5;
+        int result = App.makeModulus(numOne, numTwo);
+        Assert.assertEquals(2, result);
     }
 }
